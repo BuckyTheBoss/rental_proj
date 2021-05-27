5b819fd3c1e8fc1d2f151635749d8da3f400ede0
BuckytheBoss <buchman.avi@gmail.com>
2021-05-27 12:00:42 +0300

    updated reqs

--- pop_db.py
+++ pop_db.py
@@ -1,8 +1,12 @@
 import os
 import django
-
+from faker import Faker
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_proj.settings')
 django.setup()
 
-from rent.models import Vehicle
+from rent.models import *
+
+fake = Faker()
+
+
 
